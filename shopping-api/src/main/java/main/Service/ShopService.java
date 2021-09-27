17d1320f508201f5940d54f23ca6bb7165baa78b
Júlio Borges <gofiveqa@MacBook-Pro-de-Five.local>
2021-09-27 00:18:33 -0300

    Adds the shopping api service to project

---
+++ shopping-api/src/main/java/main/Service/ShopService.java
@@ -0,0 +1,62 @@
+package main.Service;
+
+import main.DTO.ItemDTO;
+import main.DTO.ShopDTO;
+import main.Entity.Shop;
+import main.Repository.ShopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ShopService {
+
+    @Autowired
+    private ShopRepository shopRepository;
+
+    public List<ShopDTO> getAll() {
+        List<Shop> shops = shopRepository.findAll();
+        return shops
+                .stream()
+                .map(ShopDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+    public List<ShopDTO> getByUser(String userIdentifier) {
+        List<Shop> shops = shopRepository.findAllByUserIdentifier(userIdentifier);
+        return shops
+                .stream()
+                .map(ShopDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+    public List<ShopDTO> getByDate(ShopDTO shopDTO) {
+        List<Shop> shops = shopRepository.findAllByDateGreaterThan(shopDTO.getDate());
+        return shops
+                .stream()
+                .map(ShopDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+    public ShopDTO findById(long ProductId) {
+        Optional<Shop> shop = shopRepository.findById(ProductId);
+        return shop.map(ShopDTO::convert).orElse(null);
+    }
+
+    public ShopDTO save(ShopDTO shopDTO) {
+        shopDTO.setTotal(shopDTO.getItems()
+                .stream()
+                .map(ItemDTO::getPrice)
+                .reduce((float) 0, Float::sum));
+
+        Shop shop = Shop.convert(shopDTO);
+        shop.setDate(new Date());
+        shop = shopRepository.save(shop);
+
+        return ShopDTO.convert(shop);
+    }
+}
